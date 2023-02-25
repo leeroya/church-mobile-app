3ce400bfeed5b14ac4846ef6da02d32fa8af5aad
Lee-Roy Ashworth <2323941+leeroya@users.noreply.github.com>
2023-02-25 18:18:59 +0200

    Leeroya patch 1 (#8)
    * feat: Init dotnet app
    
    * feat: Added basic auth-0 (#7)
    
    * feat: Added basic auth-0
    
    * config
    
    * Create main.yml

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # church-mobile-app
 This is a .NET MAUI application 
+
+
+https://auth0.com/blog/add-authentication-to-dotnet-maui-apps-with-auth0/
---
+++ src/DestinyLife/DestinyLife/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using DestinyLife.Auth0;
+using Microsoft.Extensions.Logging;
+
+namespace DestinyLife;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+        builder.Services.AddSingleton<MainPage>();
+
+        builder.Services.AddSingleton(new Auth0Client(new()
+        {
+            Domain = "leeroya.auth0.com",
+            ClientId = "<ID>",
+            Scope = "openid profile",
+            RedirectUri = "myapp://callback"
+        }));
+        
+		return builder.Build();
+	}
+}
